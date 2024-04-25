d008f626c674d0cb3248dce8fdcb2720815265a8
Thuee Mong Sing <thueemongs@gmail.com>
2024-04-26 02:01:36 +0600

    added footer with social links

---
+++ components/Footer.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+
+const footerData = [
+  {
+    title: 'Github',
+    link: 'https://github.com/thuee-mong-marma',
+  },
+  {
+    title: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/thuee-mong-sing-520127220/',
+  },
+];
+
+const Footer = () => {
+  return (
+    <footer className="py-10 space-y-4 text-purple">
+      <div className="text-xl text-center">Made with &#10084; by Mong</div>
+      <ul className="flex items-center justify-center gap-8">
+        {footerData.map((item, index) => (
+          <li key={index}>
+            <Link href={item.link} target="_blank" className="hover:underline">
+              {item.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
